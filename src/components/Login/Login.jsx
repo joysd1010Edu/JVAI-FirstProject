1c2956a0512accaa2dfc483936e349e106b5df2a
joysd1010Edu <joysd.1005@fec.edu.bd>
2025-07-11 06:21:37 +0600

    the authentication page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,21 @@
+"use client";
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import authenticationImage from './../../../public/authImage.png'
 import { CiMail } from "react-icons/ci";
 import { IoLockClosedOutline } from "react-icons/io5";
 import { AiFillGoogleCircle } from "react-icons/ai";
+import { IoMdEyeOff } from "react-icons/io";
+import { IoEye } from "react-icons/io5";
 
 
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
         <div>
             <div className='lg:flex items-center '>
@@ -35,7 +44,7 @@ const Login = () => {
                             <div className='mb-4 bg-white flex items-center rounded-lg'>
                                 <CiMail className='mx-2.5' size={24} />
                                 <input
-                                    className='py-3.5 font-normal border-none w-full '
+                                    className='py-3.5 font-normal focus:outline-none border-none w-full '
                                     type='text'
                                     placeholder='Email address'
                                 />
@@ -43,10 +52,21 @@ const Login = () => {
                             <div className='mb-4 bg-white flex items-center rounded-lg'>
                                 <IoLockClosedOutline className='mx-2.5' size={24} />
                                 <input
-                                    className='py-3.5 font-normal border-none w-full '
-                                    type='text'
+                                    className='py-3.5 focus:outline-none font-normal border-none w-full '
+                                    type={showPassword ? 'text' : 'password'}
                                     placeholder='Password'
                                 />
+                                <button 
+                                    type='button'
+                                    onClick={togglePasswordVisibility}
+                                    className='mx-2.5 focus:outline-none'
+                                >
+                                    {showPassword ? (
+                                        <IoMdEyeOff size={24} className='text-gray-500' />
+                                    ) : (
+                                        <IoEye size={24} className='text-gray-500' />
+                                    )}
+                                </button>
                             </div>
                             
                             <button type='submit' className='bg-[#0056F6] rounded-lg w-full text-white py-3.5 cursor-pointer mb-5'>
