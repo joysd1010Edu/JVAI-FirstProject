a6a16fb9706a133b8cc7db3dadea03b8463f7574
Your Name <nazrulislam.cse28@gmail.com>
2025-07-21 05:54:10 +0600

    prject updated

--- src/components/CommonStruggles/CommonStruggles.jsx
+++ src/components/CommonStruggles/CommonStruggles.jsx
@@ -14,7 +14,7 @@ const CommonStruggles = () => {
         <p className='lg:py-14 text-2xl lg:text-[66px]  font-serif font-bold'>You’re Not Alone and You Don’t Have <br /> to Face it <span className='italic text-blue-600'>Alone</span>.</p>
       </div>
 
-      {/*Section-2*/}
+      {/*Section-3*/}
       <div className='lg:flex gap-28 mt-28 justify-center'>
         <div>
           <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-28">
@@ -35,8 +35,8 @@ const CommonStruggles = () => {
         </div>
       </div>
 
-      {/*Section-3*/}
-      <div style={{background: "linear-gradient(253deg, rgba(2, 191, 2, 0.20) 0%, rgba(0, 68, 0, 0.20) 100%);"}} className=' lg:h-[388px] lg:flex gap-28 mt-3 lg:mt-28 justify-center items-center'>
+      {/*Section-4*/}
+      <div style={{background: "linear-gradient(253deg, rgba(1, 41, 244, 0.20) 0%, rgba(1, 12, 74, 0.20) 100%);"}} className=' lg:h-[388px] lg:flex gap-28 mt-3 lg:mt-28 justify-center items-center'>
         <div>
           <Image src='/project-image/rectangle2.png'
             alt='profile'
@@ -46,25 +46,25 @@ const CommonStruggles = () => {
         </div>
 
         <div>
-          <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-72 mt-2 lg:mt-0">
-            Dialectical Behavior Therapy (DBT)
+          <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-32 mt-2 lg:mt-0">
+            Bullying
           </h1>
-          <h1 className=' text-2xl lg:text-4xl font-semibold mb-3 mt-2.5'>Emotional regulation, <span className='text-blue-600 italic font font-playfair'>trauma,</span> <br /> borderline personality</h1>
-          <p className='text-lg'>Focuses on mindfulness, distress tolerance, emotional control, and <br /> interpersonal effectiveness.</p>
+          <h1 className=' text-2xl lg:text-4xl font-semibold mb-3 mt-2.5'>Words hurt. Silence hurts more. You <br /> don’t have to face it <span className='text-blue-600 italic'>alone.</span></h1>
+          <p className='text-lg'>Bullying leaves emotional scars  whether it happens at school, work, online, <br /> or even at home.</p>
            <Link href={'/'}><CgArrowTopRightO className='mt-3 text-4xl text-blue-600' /></Link>
         </div>
       </div>
 
 
       
-      {/*Section-4*/}
+      {/*Section-5*/}
       <div className='lg:flex gap-28 mt-28 justify-center'>
         <div>
-          <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-88">
-            Acceptance and Commitment Therapy (ACT)
+          <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-50">
+            Alcohol & Recovery
           </h1>
-          <h1 className=' text-2xl lg:text-4xl font-semibold mb-3'>Anxiety, depression, OCD, <span className='text-blue-600 italic font-semibold'>trauma,</span> <br /> life transitions</h1>
-          <p className='text-lg'>ACT focuses on psychological flexibility: the ability to stay present and <br /> choose value-driven behavior in any situation.</p>
+          <h1 className=' text-2xl lg:text-4xl font-semibold mb-3'>Every step counts and you <span className='italic text-blue-600'>don’t</span> have to take it alone.</h1>
+          <p className='text-lg'>Struggling with alcohol use or walking the path of recovery can feel overwhelming, isolating, and filled with setbacks.</p>
           <Link href={'/'}><CgArrowTopRightO className='mt-3 text-4xl text-blue-600' /></Link>
         </div>
 
@@ -76,6 +76,114 @@ const CommonStruggles = () => {
           />
         </div>
       </div>
+
+
+   
+      {/*Section-6*/}
+      <div style={{background: "linear-gradient(253deg, rgba(1, 41, 244, 0.20) 0%, rgba(1, 12, 74, 0.20) 100%);"}} className=' lg:h-[388px] lg:flex gap-28 mt-3 lg:mt-28 justify-center items-center'>
+        <div>
+          <Image src='/project-image/rectangle2.png'
+            alt='profile'
+            width={550}
+            height={550}
+          />
+        </div>
+
+        <div>
+          <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-44 mt-2 lg:mt-0">
+            Work-Life Balance
+          </h1>
+          <h1 className=' text-2xl lg:text-4xl font-semibold mb-3 mt-2.5'>When everything feels like too much <br />  it’s time to find your <span className='italic text-blue-600'>balance.</span></h1>
+          <p className='text-lg'>Long hours, endless to-do lists, and constant pressure can take a toll on <br /> your mental health. If you're feeling burned out, distracted.</p>
+           <Link href={'/'}><CgArrowTopRightO className='mt-3 text-4xl text-blue-600' /></Link>
+        </div>
+      </div>
+
+
+      
+      {/*Section-7*/}
+      <div className='lg:flex gap-28 mt-28 justify-center'>
+        <div>
+          <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-56">
+            Relationship Conflicts
+          </h1>
+          <h1 className=' text-2xl lg:text-4xl font-semibold mb-3'>Every relationship has its <span className='text-blue-600 italic'>challenges</span>  let’s work through them together.</h1>
+          <p className='text-lg'>Whether it's tension with a partner, misunderstandings with friends, or family stress that never seems to ease, relationship struggles can leave you emotionally drained and confused.</p>
+          <Link href={'/'}><CgArrowTopRightO className='mt-3 text-4xl text-blue-600' /></Link>
+        </div>
+
+        <div>
+          <Image src='/project-image/rectangle3.png'
+            alt='profile'
+            width={550}
+            height={550}
+          />
+        </div>
+      </div>
+
+      {/*Section-8*/}
+      <div style={{background: "linear-gradient(253deg, rgba(1, 41, 244, 0.20) 0%, rgba(1, 12, 74, 0.20) 100%);"}} className=' lg:h-[388px] lg:flex gap-28 mt-3 lg:mt-28 justify-center items-center'>
+        <div>
+          <Image src='/project-image/rectangle2.png'
+            alt='profile'
+            width={550}
+            height={550}
+          />
+        </div>
+
+        <div>
+          <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-32 mt-2 lg:mt-0">
+            Procrastination
+          </h1>
+          <h1 className=' text-2xl lg:text-4xl font-semibold mb-3 mt-2.5'>Stuck, overwhelmed, or <span className='text-blue-600 italic'>always</span> <br /> running out of time.</h1>
+          <p className='text-lg'>Procrastination often hides deeper issues like anxiety, perfectionism, or <br /> burnout. Infiheal helps you uncover what’s really going on.</p>
+           <Link href={'/'}><CgArrowTopRightO className='mt-3 text-4xl text-blue-600' /></Link>
+        </div>
+      </div>
+
+
+      
+      {/*Section-9*/}
+      <div className='lg:flex gap-28 mt-28 justify-center'>
+        <div>
+          <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-32">
+            Low Self-Esteem
+          </h1>
+          <h1 className=' text-2xl lg:text-4xl font-semibold mb-3'>You are enough even if it <span className='text-blue-600 italic'>doesn’t</span> feel <br /> that way right now.</h1>
+          <p className='text-lg'>Low self-esteem can make everyday decisions feel heavy, relationships feel insecure, and life feel <br /> like it’s happening without you.</p>
+        </div>
+
+        <div>
+          <Image src='/project-image/rectangle3.png'
+            alt='profile'
+            width={550}
+            height={550}
+          />
+        </div>
+      </div>
+
+      {/*Section-10*/}
+      <div style={{background: "linear-gradient(253deg, rgba(1, 41, 244, 0.20) 0%, rgba(1, 12, 74, 0.20) 100%);"}} className=' lg:h-[388px] lg:flex gap-28 mt-3 lg:mt-28 justify-center items-center'>
+        <div>
+          <Image src='/project-image/rectangle2.png'
+            alt='profile'
+            width={550}
+            height={550}
+          />
+        </div>
+
+        <div>
+          <h1 className="border border-white text-white mb-2 rounded-sm text-center font-semibold py-1 w-32 mt-2 lg:mt-0">
+            Life in General
+          </h1>
+          <h1 className=' text-2xl lg:text-4xl font-semibold mb-3 mt-2.5'>Sometimes, it’s just... everything.</h1>
+          <p className='text-lg'>You might not have a specific diagnosis. Maybe it’s not one big issue just <br /> stress, confusion, or feeling stuck.</p>
+           <Link href={'/'}><CgArrowTopRightO className='mt-3 text-4xl text-blue-600' /></Link>
+        </div>
+      </div>
+
+
+    
 
 
     </div>
